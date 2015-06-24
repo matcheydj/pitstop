d8a84b803212218a33d04d511ee89735bf45789c
Lucas Vocos <lucas.vocos@me.com>
2015-06-24 14:32:32 -0400

    modified Paperclip initializer to show images on Heroku

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :reviews
 
-	has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }
+	has_attached_file :image, 
+					  styles: { medium: "700x500#", small: "350x250>" }
+					  storage: :s3,
+                  	  :s3_credentials => "#{Rails.root}/config/s3.yml",
+                  	  :path => "/images/:id/:style.:extension",
+                  	  :url => ":s3_domain_url"
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
