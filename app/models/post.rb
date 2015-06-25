62a9023ce8ce832f58d4659f337bbaf01f9e44d5
Lucas Vocos <lucas.vocos@me.com>
2015-06-25 16:17:27 -0400

    fix syntax error in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,6 @@ class Post < ActiveRecord::Base
 	has_attached_file :image, 
 					  styles: { medium: "700x500#", small: "350x250>" },
 					  storage: :s3,
-                  	  :path => "/images/:id/:style.:extension",
+                  	  :path => "/images/:id/:style.:extension"
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
